e3f35513c156d281bc5dbd71af30170491b77ed2
swfightforyl <swfightforyl@163.com>
2017-12-12 15:06:35 +0800

    no message

---
+++ Assets/Standard Assets/2D/Scripts/ArmRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmRotation : MonoBehaviour {
+
+    public int rotationOffset = 90;
+    // Update is called once per frame
+    void Update () {
+        Vector3 differance = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        differance.Normalize();
+        float rotZ = Mathf.Atan2(differance.y, differance.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + rotationOffset);
+    }
+}
--- Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
+++ Assets/Standard Assets/2D/Scripts/PlatformerCharacter2D.cs
@@ -23,7 +23,7 @@ namespace UnitySampleAssets._2D
         private float ceilingRadius = .01f; // Radius of the overlap circle to determine if the player can stand up
         private Animator anim; // Reference to the player's animator component.
 		private Rigidbody2D playerRigidbody2D;
-
+        private Transform playerGraphics;
 
         private void Awake()
         {
@@ -32,6 +32,7 @@ namespace UnitySampleAssets._2D
             ceilingCheck = transform.Find("CeilingCheck");
             anim = GetComponent<Animator>();
 			playerRigidbody2D = GetComponent<Rigidbody2D> ();
+            playerGraphics = transform.Find("Graphics");
         }
 
 
@@ -99,9 +100,9 @@ namespace UnitySampleAssets._2D
             facingRight = !facingRight;
 
             // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+            Vector3 theScale = playerGraphics.localScale;
             theScale.x *= -1;
-            transform.localScale = theScale;
+            playerGraphics.localScale = theScale;           
         }
     }
-}
+} 
