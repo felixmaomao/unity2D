aa3f0976f933f513d90d9075fd2890e8be5df67b
swfightforyl <swfightforyl@163.com>
2017-12-12 15:52:31 +0800

    tiling

---
+++ Assets/Standard Assets/2D/Scripts/Tiling.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+namespace Assets.Standard_Assets._2D.Scripts
+{
+    public class Tiling: MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+
+        private void Update()
+        {
+                
+        }
+    }
+}
