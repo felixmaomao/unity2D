8eacd1f7fbccc8a8a8118171f3eae1653a6e217c
swfightforyl <swfightforyl@163.com>
2018-01-02 20:31:10 +0800

    pathfinding

---
+++ Assets/Standard Assets/2D/Scripts/EnemyAI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Pathfinding;
+using System.Collections.Generic;
+
+namespace Assets.Standard_Assets._2D.Scripts
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(Seeker))]
+    public class EnemyAI : MonoBehaviour
+    {
+        public Transform target;
+        public float updateRate = 2f;
+
+        private Seeker seeker;
+        private 
+    }
+}
