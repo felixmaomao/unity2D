9258443e6e98f84a7bad5200d9c874b7c5ca9e4a
swfightforyl <swfightforyl@163.com>
2018-01-10 08:16:43 +0800

    wave spawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    [System.Serializable]
+    public class Wave
+    {
+        private string name;
+        private Transform enemyPrefab;
+        private int enemyCount;
+    }
+
+    [SerializeField]
+    private List<Wave> waves;
+
+    private GameObject[] waveSpawnPoints;
+
+    private void Start()
+    {
+        waveSpawnPoints = GameObject.FindGameObjectsWithTag("WaveSpawnPoint") as GameObject[];
+        if (waveSpawnPoints==null)
+        {
+
+        }
+    }
+}
