602da6e8faa733c65eb826d67647e73abd5c7a61
swfightforyl <swfightforyl@163.com>
2018-01-24 21:36:35 +0800

    player2

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl2.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl2.cs
@@ -4,21 +4,21 @@ namespace UnitySampleAssets._2D
 {
 
     [RequireComponent(typeof (PlatformerCharacter2D))]
-    public class Platformer2DUserControl : MonoBehaviour
+    public class Platformer2DUserControl2 : MonoBehaviour
     {
         private PlatformerCharacter2D character;
         private bool jump;
 
         private void Awake()
         {
-            character = GameObject.Find("Player1").GetComponent<PlatformerCharacter2D>();
+            character = GameObject.Find("Player2").GetComponent<PlatformerCharacter2D>();
         }
 
         private void Update()
         {
             if(!jump)
             // Read the jump input in Update so button presses aren't missed.
-			if(Input.GetKeyDown(KeyCode.W)){                  
+			if(Input.GetKeyDown(KeyCode.UpArrow)){                  
 				jump = true;
 			}
            
@@ -27,7 +27,7 @@ namespace UnitySampleAssets._2D
         private void FixedUpdate()
         {
             // Read the inputs.
-            bool crouch = Input.GetKey(KeyCode.LeftControl);
+            bool crouch = Input.GetKey(KeyCode.LeftArrow);
 			float h = Input.GetAxis("Horizontal");
             // Pass all parameters to the character control script.
             character.Move(h, crouch, jump);
